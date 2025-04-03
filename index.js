ff6a5c20422719c448b0e94795d34bbdd1be8147
Leandro Fugioka <77177218+leofugioka@users.noreply.github.com>
2025-04-03 19:00:50 -0300

    Added reset functionality

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
+const resetButton = document.querySelector("#reset");
 const circle = document.querySelector("#circle");
 
 const perimeter = circle.getAttribute("r") * 2 * Math.PI;
 circle.setAttribute("stroke-dasharray", perimeter);
 
 let duration;
-const timer = new Timer(durationInput, startButton, pauseButton, {
+let isReset = false;
+const timer = new Timer(durationInput, startButton, pauseButton, resetButton, {
   onStart(totalDuration) {
-    duration = totalDuration;
+    if (!duration || isReset) duration = totalDuration;
   },
   onTick(timeRemaining) {
     circle.setAttribute("stroke-dashoffset", (perimeter * timeRemaining) / duration - perimeter);
+  },
+  onReset(resetDuration) {
+    isReset = true;
+    duration = resetDuration;
+    circle.setAttribute("stroke-dashoffset", 0);
   },
   onComplete() {
     alert("Timer completed");
--- timer.js
+++ timer.js
@@ -1,26 +1,38 @@
 class Timer {
-  constructor(durationInput, startButton, pauseButton, callbacks) {
+  initialTime = 0;
+
+  constructor(durationInput, startButton, pauseButton, resetButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+    this.resetButton = resetButton;
     if (callbacks) {
       this.onStart = callbacks.onStart;
       this.onTick = callbacks.onTick;
+      this.onReset = callbacks.onReset;
       this.onComplete = callbacks.onComplete;
     }
 
     this.startButton.addEventListener("click", this.start);
     this.pauseButton.addEventListener("click", this.pause);
+    this.resetButton.addEventListener("click", this.reset);
   }
 
   start = () => {
-    if (this.onStart) this.onStart(this.timeRemaining);
+    this.initialTime = parseFloat(this.timeRemaining);
+    if (this.onStart) this.onStart(this.initialTime);
     this.tick();
     this.interval = setInterval(this.tick, 10);
   };
 
   pause = () => {
     clearInterval(this.interval);
+  };
+
+  reset = () => {
+    this.pause();
+    this.timeRemaining = this.initialTime;
+    if (this.onTick) this.onReset(this.timeRemaining);
   };
 
   tick = () => {
@@ -40,5 +52,13 @@ class Timer {
 
   set timeRemaining(time) {
     this.durationInput.value = time.toFixed(2);
+  }
+
+  get initialTime() {
+    return this.initialTime;
+  }
+
+  set initialTime(time) {
+    this.initialTime = time.toFixed(2);
   }
 }
